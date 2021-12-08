ce54a44382c5b89bd3fe491e2e0846b7a96dcb3e
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-12-08 01:03:13 +0000

    04-10_change_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  # get 'users/index'
+  # get 'users/index', to: 'users#index'
+  get 'users', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
